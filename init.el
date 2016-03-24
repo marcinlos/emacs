eec09c08963ec18ac4110ec4a20d95fd9d5aab59
Marcin Łoś <marcin.los.91@gmail.com>
2016-03-24 18:37:51 +0100

    Add org-mode keybindings

--- init.el
+++ init.el
@@ -70,6 +70,15 @@
 ;; Better expanding
 (global-set-key (kbd "M-/") 'hippie-expand)
 
+;; Org mode bindings
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+(setq org-completion-use-ido t)
+(setq org-outline-path-complete-in-steps nil)
+
+
 ;; Override marked text
 (delete-selection-mode)
 
