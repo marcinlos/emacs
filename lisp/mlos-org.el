59f1872314999665bd43dc48be200c9df6f30c08
Marcin Łoś <marcin.los.91@gmail.com>
2016-03-28 01:05:38 +0200

    use-packagize org-mode configuration

--- lisp/mlos-org.el
+++ lisp/mlos-org.el
@@ -1,16 +1,20 @@
 ;;; Org mode configuration
 
-;; Org mode bindings
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
+(use-package org
+  :ensure t
+  :config
+  ; global bindings
+  (global-set-key (kbd "C-c l") 'org-store-link)
+  (global-set-key (kbd "C-c c") 'org-capture)
+  (global-set-key (kbd "C-c a") 'org-agenda)
+  (global-set-key (kbd "C-c b") 'org-iswitchb)
 
-(setq org-completion-use-ido t)
-(setq org-outline-path-complete-in-steps nil)
+  (setq org-completion-use-ido t)
+  (setq org-outline-path-complete-in-steps nil)
 
-(add-hook 'org-mode-hook
-          (lambda () (electric-indent-mode -1)))
+  ; disable electric-indent as it causes problems with org
+  (add-hook 'org-mode-hook
+            (lambda () (electric-indent-mode -1))))
 
 
 (provide 'mlos-org)
