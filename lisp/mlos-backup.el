f0daf629fdab6c22e1581c938d9cda8b4debb48d
Marcin Łoś <marcin.los.91@gmail.com>
2016-03-27 22:08:02 +0200

    Create backup file directory if it does not exist

--- lisp/mlos-backup.el
+++ lisp/mlos-backup.el
@@ -3,6 +3,11 @@
 ;; Backups in separate directory
 (defconst mlos-backup-dir "~/.emas-backups" "Backup directory")
 
+;; Create it if it does not exist
+(unless (file-exists-p mlos-backup-dir)
+  (make-directory mlos-backup-dir))
+
+
 ;; Backup files (by default sth~)
 (setq backup-directory-alist `((".*" . ,mlos-backup-dir)))
 
