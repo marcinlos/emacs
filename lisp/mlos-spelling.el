dc18f1076ffc1e22b1da3ecefb4f3d40ebc60b77
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-02 22:38:53 +0200

    Change Flyspell configuration to use use-package

--- lisp/mlos-spelling.el
+++ lisp/mlos-spelling.el
@@ -1,14 +1,15 @@
 ;;;; Flyspell configuration
 
-;; Enable ordinary Flyspell for text modes
-(add-hook 'text-mode-hook 'flyspell-mode)
 
-;; Enable comment-and-string-only mode for program modes
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(use-package flyspell
+  :diminish flyspell-mode
+  :diminish flyspell-prog-mode
 
-;; Diminish both modes
-(diminish 'flyspell-mode)
-(diminish 'flyspell-prog-mode)
+  :config
+  ; Enable ordinary Flyspell for text modes
+  (add-hook 'text-mode-hook 'flyspell-mode)
+  ; Enable comment-and-string-only mode for program modes
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode))
 
 
 (provide 'mlos-spelling)
