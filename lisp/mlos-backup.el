e736499319784af0b1c60f15fc2d69cd095a7888
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-03 11:24:30 +0200

    Fix backup path
    Auto save directory seems to require terminating slash

--- lisp/mlos-backup.el
+++ lisp/mlos-backup.el
@@ -1,18 +1,20 @@
 ;;; Backup configuration
 
 ;; Backups in separate directory
-(defconst mlos-backup-dir "~/.emacs-backups" "Backup directory")
+(defconst mlos/backup-dir
+  (expand-file-name "~/.emacs-backups/")
+  "Backup directory")
 
 ;; Create it if it does not exist
-(unless (file-exists-p mlos-backup-dir)
-  (make-directory mlos-backup-dir))
+(unless (file-exists-p mlos/backup-dir)
+  (make-directory mlos/backup-dir))
 
 
 ;; Backup files (by default sth~)
-(setq backup-directory-alist `((".*" . ,mlos-backup-dir)))
+(setq backup-directory-alist `((".*" . ,mlos/backup-dir)))
 
 ;; Autosave files (by default #sth#)
-(setq auto-save-file-name-transforms `((".*" ,mlos-backup-dir t)))
+(setq auto-save-file-name-transforms `((".*" ,mlos/backup-dir t)))
 
 
 (provide 'mlos-backup)
