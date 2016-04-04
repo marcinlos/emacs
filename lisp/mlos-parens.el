1946050180d550ba0d0f798e7d59efd7a268f21e
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-04 19:21:00 +0200

    Add rainbow-delimiters

--- lisp/mlos-parens.el
+++ lisp/mlos-parens.el
@@ -4,5 +4,11 @@
   :ensure t
   :diminish "")
 
+;; Highlight brackets according to their depth
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
 
 (provide 'mlos-parens)
