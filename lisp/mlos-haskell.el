1237918afe50097247a3e764324a45a575ff0fc6
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-10 19:52:02 +0200

    Upgrade haskell configuration
    Mostly make use of deferred loading to shorten load times. Also, clean
    up few issues (like keybindings).

--- lisp/mlos-haskell.el
+++ lisp/mlos-haskell.el
@@ -5,12 +5,10 @@
 (let ((mlos/cabal-path (expand-file-name "~/.cabal/bin")))
   (setenv "PATH" (concat mlos/cabal-path ":" (getenv "PATH")))
   (add-to-list 'exec-path mlos/cabal-path))
-
-(use-package flycheck-haskell
-  :ensure t)
-
 (use-package haskell-mode
   :ensure t
+  :commands haskell-mode
+
   :config
 
   (custom-set-variables
@@ -18,76 +16,87 @@
    '(haskell-process-auto-import-loaded-modules t)
    '(haskell-process-log t))
 
-  ;; Use cabal-repl with ghci-ng
+  ;; Use cabal-repl
   (custom-set-variables '(haskell-process-type 'cabal-repl))
-  (setq haskell-process-args-cabal-repl
-        '("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng"))
+
+  ;; ... with ghci-ng if possible
+  (when (executable-find "ghci-ng")
+    (setq haskell-process-args-cabal-repl
+          '("--ghc-option=-ferror-spans" "--with-ghc=ghci-ng")))
 
   (defun mlos/haskell-mode-hook ()
     ;; enable interactive mode
     (interactive-haskell-mode)
     ;; enable enhanced documentation
-    (turn-on-haskell-doc)
+    (haskell-doc-mode)
     ;; insert module template into a newly created module file
     (haskell-auto-insert-module-template)
-    ;; flycheck support
-    (flycheck-mode)
-    (flycheck-haskell-setup)
     ;; enable moving by declaration
     (haskell-decl-scan-mode)
 
-    ;; enable code completion
-    (company-mode)
+    ;; flycheck support
+    (flycheck-mode)
     ;; sub-word movement
-    (subword-mode)
-    ;; diminish minor modes
-    (diminish 'haskell-doc-mode)
-    (diminish 'interactive-haskell-mode))
+    (subword-mode))
 
   (add-hook 'haskell-mode-hook #'mlos/haskell-mode-hook)
 
-  ;; Key bindings
+    ;; haskell mode bindings
+  :bind (:map haskell-mode-map
+              ("C-c c"   . haskell-process-cabal)))
 
-  ;; haskell bindings
-  (with-eval-after-load 'haskell-mode
-    (bind-keys :map haskell-mode-map
-               ("C-c C-l" . haskell-process-load-file)
-               ("C-c C-z" . haskell-interactive-switch)
-               ("C-c C-t" . haskell-process-do-type)
-               ("C-c C-i" . haskell-process-do-info)
-               ("C-c c"   . haskell-process-cabal)
-               ("C-c C-c" . haskell-process-cabal-build))
-    (bind-key "M-/" #'company-complete haskell-mode-map))
 
-  ;; haskell interactive mode bindings
-  (with-eval-after-load 'interactive-haskell-mode
-    (bind-keys :map interactive-haskell-mode-map
-               ("C-c C-t" . haskell-mode-show-type-at)
-               ("M-."     . haskell-mode-goto-loc)))
+;; Configuration of the interactive mode
+(use-package haskell
+  :commands interactive-haskell-mode
+  :diminish interactive-haskell-mode
 
-  ;; cabal file bindings
-  (with-eval-after-load 'haskell-cabal
-    (bind-keys :map haskell-cabal-mode-map
-               ("C-c C-z" . haskell-interactive-switch)
-               ("C-c C-k" . haskell-interactive-mode-clear)
-               ("C-c c"   . haskell-process-cabal)
-               ("C-c C-c" . haskell-process-cabal-build))))
+  :bind (:map interactive-haskell-mode-map
+              ("C-c C-t" . haskell-mode-show-type-at)
+              ("M-."     . haskell-mode-goto-loc)))
+
+
+;; cabal file editing mode
+(use-package haskell-cabal
+  :bind (:map haskell-cabal-mode-map
+              ("C-c C-z" . haskell-interactive-switch)
+              ("C-c C-k" . haskell-interactive-mode-clear)
+              ("C-c c"   . haskell-process-cabal)
+              ("C-c C-c" . haskell-process-cabal-build)))
+
+;; Flycheck checker for haskell
+(use-package flycheck-haskell
+  :ensure t
+  :commands flycheck-haskell-setup
+  :init
+  (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
+
+;; like ElDoc, for haskell
+(use-package haskell-doc
+  :diminish haskell-doc-mode
+  :defer)
+
 
 ;; hi2 intendation
 (use-package hi2
   :ensure t
+  :commands turn-on-hi2
   :diminish 'hi2-mode
-  :config
+  :init
   (add-hook 'haskell-mode-hook #'turn-on-hi2)
+  :config
   ;; disable visual indicators
   (setq hi2-show-indentations nil))
 
 ;; Backend for company
 (use-package company-ghci
   :ensure t
-  :config
-  (push 'company-ghci company-backends)
-  (add-hook 'haskell-mode-hook #'company-mode))
+  :commands company-ghci
+  :init
+  (defun mlos/haskell-add-ghci-backend ()
+    "Add `company-ghci' to the list of company backends"
+    (push 'company-ghci company-backends))
+  (add-hook 'haskell-mode-hook #'mlos/haskell-add-ghci-backend))
 
 
 (provide 'mlos-haskell)
