df863543108749016bbbc6cd4086ed3fea358b44
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-10 22:14:08 +0200

    Reconfigure git bindings

--- lisp/mlos-magit.el
+++ lisp/mlos-magit.el
@@ -2,10 +2,15 @@
 
 (use-package magit
   :ensure t
-  :bind (("C-x g" . magit-status)))
+  :config
+
+  :bind (("C-c v v" . magit-status)
+         ("C-c v b" . magit-blame)
+         ("C-c v i" . magit-gitignore)))
 
 ;; Move between versions of a file
 (use-package git-timemachine
-  :ensure t)
+  :ensure t
+  :bind (("C-c v t" . git-timemachine)))
 
 (provide 'mlos-magit)
