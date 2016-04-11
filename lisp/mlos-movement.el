45fb45e86b78c89105375376aca371f4dd4aa299
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-11 23:32:42 +0200

    Improve avy bindings

--- lisp/mlos-movement.el
+++ lisp/mlos-movement.el
@@ -22,9 +22,17 @@
 ;; Jump to location marked by letter
 (use-package avy
   :ensure t
-  :bind (("C-'"   . avy-goto-char-2)
-         ("C-\""  . avy-goto-char-timer)
-         ("C-M-'" . avy-pop-mark)))
+  :init
+  (mlos/describe-keys "C-c j" "jump")
+  :config
+  (setq avy-keys (string-to-list "asdfjklqwecviopnmughr"))
+  :bind (("C-'"     . avy-goto-char-2)
+         ("C-\""    . avy-goto-char-timer)
+         ("C-M-'"   . avy-pop-mark)
+         ("C-c j j" . avy-goto-char-2)
+         ("C-c j k" . avy-goto-word-or-subword-1)
+         ("C-c j l" . avy-goto-line)
+         ("C-c j ," . avy-pop-mark)))
 
 ;; Follow links marked by letter
 (use-package ace-link
@@ -35,7 +43,8 @@
 ;; Jump/delete window marked by letter
 (use-package ace-window
   :ensure t
-  :bind (("M-O" . ace-window))
+  :bind (("M-O"     . ace-window)
+         ("C-c j o" . ace-window))
   :config
   ;; use home row to address windows
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
