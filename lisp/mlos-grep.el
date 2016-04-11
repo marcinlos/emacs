f35314aacbe85e108ff8949dec5da7e6a1eeedef
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-12 01:54:18 +0200

    Add bindings for ag

--- lisp/mlos-grep.el
+++ lisp/mlos-grep.el
@@ -9,13 +9,21 @@
 ;; Use Ag if possible
 (when (executable-find "ag")
   (use-package ag
+    :ensure t
     :defer t
-    :ensure t)
+    :init
+    (mlos/describe-keys "C-c s" "ag")
+    :config
+    (setq ag-highlight-search t)
+
+    :bind (("C-c s s" . ag)
+           ("C-c s f" . ag-files)
+           ("C-c s r" . ag-regexp)
+           ("C-c s d" . ag-dired-regexp)))
+
   ;; writable results
   (use-package wgrep-ag
     :ensure t
-    :defer t
-    :config
-    (setq ag-highlight-search t)))
+    :defer t))
 
 (provide 'mlos-grep)
