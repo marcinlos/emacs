7d6a0ab9c4e6d49edcb5ba45726760c47a86a502
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-12 01:11:02 +0200

    Add eval-sexp-fu (highlight eval'd code)

--- lisp/mlos-elisp.el
+++ lisp/mlos-elisp.el
@@ -5,6 +5,13 @@
                      lisp-interaction-mode-hook
                      ielm-mode-hook)))
 
+  (use-package eval-sexp-fu
+    :ensure t
+    :commands eval-sexp-fu-flash-mode
+    :config
+    (set-face-bold 'eval-sexp-fu-flash nil)
+    (set-face-bold 'eval-sexp-fu-flash-error nil))
+
   ;; symbol navigation as in SLIME
   (use-package elisp-slime-nav
     :ensure t
@@ -15,7 +22,8 @@
     "Enable tools for editing Emacs Lisp code"
     (eldoc-mode)
     (paredit-mode)
-    (elisp-slime-nav-mode))
+    (elisp-slime-nav-mode)
+    (eval-sexp-fu-flash-mode))
 
   (dolist (m elisp-modes)
     (add-hook m #'mlos/setup-elisp-modes)))
