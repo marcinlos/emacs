bbdf809c273a45afc27658fe09c5f38ac8669649
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-25 02:59:53 +0200

    Improve scheme configuration

--- lisp/mlos-scheme.el
+++ lisp/mlos-scheme.el
@@ -1,18 +1,26 @@
 ;;; Scheme configuration
 
-(use-package scheme
-  :defer t
-  :init
-  (defun mlos/setup-scheme-modes ()
+(defun mlos/setup-scheme-modes ()
     (paredit-mode)
     (eldoc-mode)
     (flycheck-mode))
 
+(use-package scheme
+  :defer t
+  :init
   (add-hook 'scheme-mode-hook #'mlos/setup-scheme-modes))
+
+(use-package quack
+  :ensure t
+  :defer t)
 
 (use-package geiser
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  (setq geiser-default-implementation 'racket)
+  (add-hook 'geiser-repl-mode-hook #'mlos/setup-scheme-modes)
+  :bind (("C-c C-l" . run-geiser)))
 
 (use-package geiser-autodoc
   :diminish geiser-autodoc-mode
