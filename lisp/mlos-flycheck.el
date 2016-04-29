d52f665f2f2362453db113e5dbf7c9800a03550e
Marcin Łoś <marcin.los.91@gmail.com>
2016-04-30 01:22:54 +0200

    Display flycheck messages in tooltip window

--- lisp/mlos-flycheck.el
+++ lisp/mlos-flycheck.el
@@ -5,4 +5,12 @@
   :commands flycheck
   :diminish " ✓ ")
 
+;; Display flycheck messages in a tooltip window
+(use-package flycheck-pos-tip
+  :ensure t
+  :commands flycheck-pos-tip-mode
+  :init
+  (with-eval-after-load 'flycheck
+    (flycheck-pos-tip-mode)))
+
 (provide 'mlos-flycheck)
