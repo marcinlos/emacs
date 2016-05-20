df1af0fb59237fe66999433312ca0e3340ad74c6
Marcin Łoś <marcin.los.91@gmail.com>
2016-05-20 18:50:49 +0200

    Change word wrap handling for LaTeX and bibtex

--- lisp/mlos-latex.el
+++ lisp/mlos-latex.el
@@ -11,14 +11,19 @@
 
   (defun mlos/latex-mode-hook ()
     (turn-on-reftex)
-    ;; paragraphs are filled to 80 characters
-    (turn-on-auto-fill)
-    (set-fill-column 80)
+    ;; one sentence per line
+    (visual-line-mode)
     ;; spell checker
     (require 'flyspell)
     (flyspell-mode t))
 
   (add-hook 'LaTeX-mode-hook #'mlos/latex-mode-hook))
 
+(use-package bibtex
+  :defer t
+  :init
+  (defun mlos/bibtex-mode-hook ()
+    (visual-line-mode))
+  (add-hook 'bibtex-mode-hook #'mlos/bibtex-mode-hook))
 
 (provide 'mlos-latex)
