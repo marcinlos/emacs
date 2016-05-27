32122ba153166455ffb967e9f0bf06206fbfbfab
Marcin Łoś <marcin.los.91@gmail.com>
2016-05-27 20:00:13 +0200

    Use flyspell-correct, clean up bindings

--- lisp/mlos-spelling.el
+++ lisp/mlos-spelling.el
@@ -5,15 +5,22 @@
   :diminish flyspell-mode
   :diminish flyspell-prog-mode
 
-  :init
-  ;; Rebind flyspell-auto-correct-previous-word
-  (setq flyspell-auto-correct-binding (kbd "C-c ;"))
-
   :config
+  ;; don't pollute the binding space
+  (dolist (key '("C-c $" "C-;" "C-,"))
+    (unbind-key key flyspell-mode-map))
   ;; Enable ordinary Flyspell for text modes
   (add-hook 'text-mode-hook #'flyspell-mode)
   ;; Enable comment-and-string-only mode for program modes
   (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+
+(use-package flyspell-correct
+  :ensure t
+  :defer t
+  :config
+  (setq flyspell-correct-interface 'flyspell-correct-helm)
+  :bind (:map flyspell-mode-map
+              ("C-c ;" . flyspell-correct-word-generic)))
 
 
 (provide 'mlos-spelling)
